9e8c30af8082aea6887d397c7d70de6a1d0a4f79
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-10-02 18:20:52 +0200

    Création entités projet et offre d'emploi

---
+++ src/Entity/Projet.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjetRepository::class)
+ */
+class Projet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $objet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $estRecontacte;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getObjet(): ?string
+    {
+        return $this->objet;
+    }
+
+    public function setObjet(string $objet): self
+    {
+        $this->objet = $objet;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEstRecontacte(): ?bool
+    {
+        return $this->estRecontacte;
+    }
+
+    public function setEstRecontacte(bool $estRecontacte): self
+    {
+        $this->estRecontacte = $estRecontacte;
+
+        return $this;
+    }
+}
