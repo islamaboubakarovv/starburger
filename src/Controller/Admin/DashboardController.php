52e9a9dc57779b8e952ebb4743de4c377a42cd48
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-10-03 11:16:24 +0200

    Ajout de toutes les entités sur easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller\Admin;
 
+
+use App\Entity\Article;
+use App\Entity\Artisan;
+use App\Entity\Client;
+use App\Entity\Offre;
+use App\Entity\Postulant;
+use App\Entity\IllustrationArticle;
+use App\Entity\Projet;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,6 +35,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Articles', 'fas fa-list', Article::class);
+        yield MenuItem::linkToCrud('Artisans', 'fas fa-list', Artisan::class);
+        yield MenuItem::linkToCrud('Clients', 'fas fa-list', Client::class);
+        yield MenuItem::linkToCrud('Offres', 'fas fa-list', Offre::class);
+        yield MenuItem::linkToCrud('Postulants', 'fas fa-list', Postulant::class);
+        yield MenuItem::linkToCrud('Projets', 'fas fa-list', Projet::class);
+        yield MenuItem::linkToCrud('Images d\'articles', 'fas fa-list', IllustrationArticle::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
