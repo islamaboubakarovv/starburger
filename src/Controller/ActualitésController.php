1dec34012e849d0179e45dfba36e455f94a82b15
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-10-20 22:18:40 +0200

    Modification pages actualités et articles

--- src/Controller/ActualitésController.php
+++ src/Controller/ActualitésController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ActualitésController extends AbstractController
 {
     #[Route('/actualites', name: 'actualites')]
-    public function index(): Response
-    {
-        return $this->render('actualités/index.html.twig', [
-            'controller_name' => 'ActualitésController',
-        ]);
+    public function index(Environment $twig, ArticleRepository $articleRepository): Response
+     {
+        return new Response($twig->render('actualités/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+            ]));
     }
 }
