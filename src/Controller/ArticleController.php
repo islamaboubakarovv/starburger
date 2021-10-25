2ccd154ef624123a430c3b44d3427141ef280802
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-10-25 20:43:15 +0200

    Ajout des polices personnalisée et affichage titre et contenu en fct id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\IllustrationArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'article')]
-    public function index(): Response
-    {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
+    public function index(Environment $twig, ArticleRepository $articleRepository, IllustrationArticleRepository $illustrationarticle): Response
+     {
+        return new Response($twig->render('article/index.html.twig', [
+            'articles' => $articleRepository->find($_GET['id']),
+            ]));
     }
 }
