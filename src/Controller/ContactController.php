7dac8af57b6030a7c9b3221d44fb4124f01f2313
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-11-02 11:25:39 +0100

    Formulaire demande devis page nous contacter totalement fonctionnel

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,14 +7,27 @@ use App\Form\DemandeProjetType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request)
     {
         $projet = new Projet();
         $form = $this->createForm(DemandeProjetType::class, $projet);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $projet->setestRecontacte(0);
+            
+            $em->persist($projet);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre devis a été envoyé !');
+        }
 
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
--- src/Form/DemandeProjetType.php
+++ src/Form/DemandeProjetType.php
@@ -7,14 +7,31 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DemandeProjetType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('objet')
-            ->add('description')
+            ->add('objet', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                   new NotBlank([
+                      'message' => 'Veuillez saisir un objet'
+                   ]),
+                ]
+             ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'constraints' => [
+                   new NotBlank([
+                      'message' => 'Veuillez saisir une description'
+                   ]),
+                ]
+             ])
             ->add('valider', SubmitType::class)
         ;
     }
