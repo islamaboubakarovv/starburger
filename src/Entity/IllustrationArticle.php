165c3347c7a2d7a4c442902d4be689468ee54c3b
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-11-06 16:00:08 +0100

    Début implémentation VichUploader (ça marche pas snif)

--- src/Entity/IllustrationArticle.php
+++ src/Entity/IllustrationArticle.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use App\Repository\IllustrationArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=IllustrationArticleRepository::class)
+ * @Vich\Uploadable
  */
 class IllustrationArticle
 {
@@ -27,6 +30,24 @@ class IllustrationArticle
      * @ORM\JoinColumn(nullable=false, onDelete="CASCADE")
      */
     private $article;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="article_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -55,5 +76,33 @@ class IllustrationArticle
         $this->article = $article;
 
         return $this;
+    }
+    
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
     }
 }
