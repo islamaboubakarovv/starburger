3b8fbaacb2f557c15924d7c8e27eb9ff25c6536e
Lucas <lucas.delaperriere@gmail.com>
2021-11-09 15:27:13 +0100

    Peauffinage accueil, reste visionneuse d'image à terminer

--- assets/base.js
+++ assets/base.js
@@ -1,32 +1,26 @@
 $(function(){
-              $('.navbar').data('size','big');
-          });
+    $('.navbar').data('size','big');
+});
 
-          $(window).scroll(function(){
-              if($(document).scrollTop() > 0)
-              {
-                  if($('.navbar').data('size') == 'big')
-                  {
-                      $('.navbar').data('size','small');
-                      $('.navbar-brand img').stop().animate({
-                          height:'50px'
-                      },600);
-                      $(".navbar").stop().animate({
-                        "font-size": "0.8rem"
-                      }, 600, "swing");
-                  }
-              }
-              else
-              {
-                  if($('.navbar').data('size') == 'small')
-                  {
-                      $('.navbar').data('size','big');
-                      $('.navbar-brand img').stop().animate({
-                          height:'70px'
-                      },600);
-                      $(".navbar").stop().animate({
-                        "font-size": "1rem"
-                      }, 600, "swing");
-                    }  
-              }
-          });
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('.navbar').data('size') == 'big')
+        {
+            $('.navbar').data('size','small');
+            $('.navbar-brand img').stop().animate({
+                height:'50px'
+            },800);
+        }
+    }
+    else
+    {
+        if($('.navbar').data('size') == 'small')
+        {
+            $('.navbar').data('size','big');
+            $('.navbar-brand img').stop().animate({
+                height:'70px'
+            },800);
+        }
+    }
+});
