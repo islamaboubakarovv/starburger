35ea424334fa077813d5962837da3d77dfd910c3
Anthony Merime <anthony.mrim@gmail.com>
2021-11-12 11:34:19 +0100

    Bug route fix + Modification formulaire

--- src/Form/CandidatureType.php
+++ src/Form/CandidatureType.php
@@ -6,20 +6,24 @@ use App\Entity\Postulant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CandidatureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // ->add('offre')
-            ->add('postulant')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('mail')
-            ->add('objet')
-            ->add('message')
-            ->add('valider')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('telephone', NumberType::class)
+            ->add('mail', EmailType::class)
+            ->add('cv', FileType::class)
+            ->add('lettreMotivation', FileType::class)
+         //     ->add('valider',SubmitType::class)
 
         ;
     }
