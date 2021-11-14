3df6ed638aea140e98355cd149f5c44418519012
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-11-14 11:36:34 +0100

    Modif système mail et début visuel mail demande devis

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,9 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
+
 
 class ContactController extends AbstractController
 {
@@ -30,24 +31,15 @@ class ContactController extends AbstractController
             $em->flush();
 
             $dodge = $form->getData();
-            $objet_devis = $dodge->getObjet();
-            $description_devis = $dodge->getDescription();
-            $message = (new Email())
+            $message = (new TemplatedEmail())
             ->from(new Address('xsway41@gmail.com', 'ABC Legermain'))
                 ->to('julienvercoutere@yahoo.fr')
-                ->embed(fopen('images/logos/LogoArtisan.PNG', 'r'), 'logo')
                 ->subject('Vous avez reçu une nouvelle demande de devis')
-                ->text('Sender : ')
-                ->html('<img src="cid:logo" width="130px">
-                <p>Objet : '.dump($objet_devis).'</p>
-                <p>Description de la demande : '.dump($description_devis).'</p>
-                <table>
-                    <tr>
-                        <td>
-                            <a href="http://127.0.0.1:8000/accueil"><button>Accéder au site</button></a>
-                        <td>
-                    </tr>
-                </table>');
+                ->htmlTemplate('mail_devis/index.html.twig')
+                ->context([
+                    'description_devis' => $dodge->getDescription(),
+                    'objet_devis' => $dodge->getObjet(),
+                ]);
             $mailer->send($message);
 
             $this->addFlash('success', 'Votre devis a été envoyé !');
