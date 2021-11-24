73051128419498c98465f69ee241396c8e7e3e5d
Julien Vercoutere <julienvercoutere@yahoo.fr>
2021-11-24 15:14:27 +0100

    Début système export données client Vcard

--- src/Controller/Admin/ClientCrudController.php
+++ src/Controller/Admin/ClientCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Client;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 
 class ClientCrudController extends AbstractCrudController
 {
@@ -13,20 +17,32 @@ class ClientCrudController extends AbstractCrudController
         return Client::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('prenom','Prénom'),
+            TextField::new('nom','Nom'),
+            EmailField::new('mail','E-mail'),
+            TextField::new('telephone','Téléphone'),
+            TextField::new('adresse','Adresse'),
+            TextField::new('ville','Ville'),
+            TextField::new('code_postal','Code Postal'),
         ];
     }
-    */
 
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
             ->setPageTitle('index', 'Liste des clients');
     }
+
+    public function configureAssets(Assets $assets): Assets
+    {
+        return $assets
+            ->addHtmlContentToBody('<a href="/client">Exporter les données</a>')
+        ;
+    }
+
+
 }
